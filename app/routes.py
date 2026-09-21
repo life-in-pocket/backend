441f61ba81937f7a9eaadaf15442ab9b197b8f06
Hoty <bohdansav111@gmail.com>
2026-09-22 00:17:28 +0200

    Rewrite endpoint for date check

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,56 @@
-from fastapi import APIRouter, Depends, status, Response
+from fastapi import APIRouter, Depends, status, Response, HTTPException
 from typing import Annotated
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from loguru import logger
-from app.database import get_db
-from app.schema import TaskResponse
-from app.model import Task
-from app.schema import TaskCreate
+from sqlalchemy.orm import joinedload
 
+from app.database import get_db
+from app.schema import TaskResponse, DayTaskResponse, DayTaskCreate, BlockCreate
+from app.model import Task, DayTask
+from app.schema import TaskCreate
+import datetime
 
 
 router_task = APIRouter(tags=["Tasks"])
 
 SessionDep = Annotated[AsyncSession, Depends(get_db)]
 
-@router_task.get("/", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
-async def get_block(db: SessionDep):
-    stmt = select(Task)
+@router_task.get("/days/{date}/tasks", response_model=list[DayTaskResponse], status_code=status.HTTP_200_OK)
+async def get_block(date: datetime.date, db: SessionDep):
+    stmt = select(DayTask).where(DayTask.date == date).options(joinedload(DayTask.task))
     result = await db.execute(stmt)
     tasks = result.scalars().all()
     logger.info("Data successfully fetched")
     return tasks
 
-@router_task.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
-async def create_block(task: TaskCreate, db: SessionDep):
+@router_task.post("/days/day-tasks", response_model=DayTaskResponse, status_code=status.HTTP_201_CREATED)
+async def create_block(task: BlockCreate, db: SessionDep):
     new_task = Task(
         title=task.title,
+        target_default=task.target,
+    )
+    db.add(new_task)
+    await db.flush()
+
+    new_day_task = DayTask(
+        task_id=new_task.id,
+        date=datetime.date.today(),
         time=task.time,
-        target=task.target
+        target=task.target,
+    )
+    db.add(new_day_task)
+    await db.commit()
+
+    stmt = (
+        select(DayTask, Task.title)
+        .where(DayTask.id == new_day_task.id)
+        .options(joinedload(DayTask.task))
     )
 
-    db.add(new_task)
-    await db.commit()
-    await db.refresh(new_task)
-
+    result = await db.scalar(stmt)
     logger.info("Data successfully created")
-    return new_task
+    return result
 
 @router_task.put("/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 async def change_block(task_id: int, db: SessionDep, task: TaskCreate):
@@ -48,7 +63,18 @@ async def change_block(task_id: int, db: SessionDep, task: TaskCreate):
     logger.info(f"Data with id: {task_id}, successfully updated")
     return changed_task
 
+@router_task.put("/{task_id}/description", response_model=TaskResponse, status_code=status.HTTP_200_OK)
+async def update_description(task_id: int, db: SessionDep, task: TaskCreate):
+    update_task = await db.get(Task, task_id)
 
+    if update_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    update_task.description = task.description
+    await db.commit()
+    await db.refresh(update_task)
+    logger.info(f"Description with id: {task_id}, successfully updated")
+    return update_task
 
 @router_task.patch("/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 async def update_time(task_id: int, db: SessionDep, task: TaskCreate):
--- app/schema.py
+++ app/schema.py
@@ -1,10 +1,31 @@
+import datetime
 from pydantic import BaseModel
+from pydantic import Field
 
 class TaskCreate(BaseModel):
     title: str
+    target_default: float
+
+class TaskResponse(TaskCreate):
+    id: int
+
+class BlockCreate(BaseModel):
+    title: str
+    time: float
+    target: float
+    
+class DayTaskBase(BaseModel):
+    is_active: bool = True
     time: float
     target: float
     description: str | None = None
 
-class TaskResponse(TaskCreate):
+class DayTaskCreate(DayTaskBase):
+    task_id: int
+    date: datetime.date = Field(default_factory=datetime.date.today)
+
+class DayTaskResponse(DayTaskBase):
     id: int
+    task_id: int
+    date: datetime.date
+    task: TaskResponse
